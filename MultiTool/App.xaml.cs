67030d547a587158cc0dcf87c51419d2d15c6273
Dmitriy Chitnaev <chitnaev.da@nppgamma.ru>
2022-07-04 17:36:57 +0300

    Initial commit

---
+++ MultiTool/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiTool;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MultiTool/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiTool;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
